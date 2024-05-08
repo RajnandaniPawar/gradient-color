5dcd26ace5585e318bc4d1b597ce8724f5449e68
NANDANNIII <Pawarnandani1827@gmail.com>
2024-05-08 21:11:41 +0530

    gredient

---
+++ javascript100days/scriptfull.js
@@ -0,0 +1,44 @@
+//   ???????????  1st day ???????????????????????
+//   ques 1 : -longest word in string. 
+
+// const longestword = (str) => {
+//     if(str.trim().length === 0){
+//         return false;
+//     }
+
+// words = str.split(" ");
+// words = words.sort();
+// console.log(words);
+// };
+
+// console.log(
+//    longestword("watch thapa technical javascript course on youtube")
+// );
+
+
+
+
+
+
+
+// \\\\\\\\\\\\\\\\\\\\\javascript course \\\\\\\\\\\\\\\\
+// cheak  the prime no :- jo number khud se ya 1 se devide ho
+// let num = 13;
+// let isprime = true;
+// for(let i=2; i<num; i++){
+//       if(num % i == 0){
+//         isprime = false;
+//         break;
+//       }
+// }
+// if(isprime){
+//     console.log("num is prime");
+// }
+// else{
+//     console.log("num is not prime");
+// }
+
+// find the given that  year is leap year or not???????
+// creat a pattner
+// create a reverse string programmer
+// create a function is deremine if a given string is pallendrome 
