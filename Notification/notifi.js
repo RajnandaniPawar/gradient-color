7d6bfdae87fdfe99765c4d2e0c2bd4503c1ae863
NANDANNIII <Pawarnandani1827@gmail.com>
2024-07-15 00:11:58 +0530

    toastnotification

---
+++ Notification/notifi.js
@@ -0,0 +1,26 @@
+let toastBox = document.getElementById('ToastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i>Successfully submitted';
+let erroeMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid Input , Check again';
+
+function showToast(msg){
+   let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+      
+    if(msg.includes('Invalid')){
+         toast.classList.add('Invalid');
+    }
+
+    setTimeout(() => {
+        toast.remove();
+    },6000);
+
+
+}
